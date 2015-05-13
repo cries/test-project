b419a2149b11abc361a30a3f4e65a2c286d23517
Christan Ries <ries_christian@hotmail.com>
2015-05-13 17:22:54 +0200

    Initial commit

---
+++ src/main/java/de/fhkoeln/gm/eis/ss15/App.java
@@ -0,0 +1,13 @@
+package de.fhkoeln.gm.eis.ss15;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
